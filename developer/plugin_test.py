3975d455d4bc67ed458dbfcd39c6f8a71c7be4fd
SuperGuGuGu <13680478000@163.com>
2024-01-10 20:44:41 +0800

    0.3.1 添加妙妙小工具

---
+++ developer/plugin_test.py
@@ -0,0 +1,28 @@
+import asyncio
+import sys
+import time
+from nonebot import logger
+sys.path.append("..")
+from plugins.plugin_kanonbot_for_Kook.plugins import plugin_jellyfish_box
+
+# #########################
+# 无需运行bot就能测试插件的小工具
+# 方便进行插件调试
+# 如果填写的配置的路径开头为"./"，会在此文件所在文件夹生成一份新配置文件夹
+# #########################
+
+async def run():
+    data = await plugin_jellyfish_box(
+        user_id="KnTest",
+        user_name="KnTest",
+        channel_id="test_channel",
+        msg="水母箱",
+        time_now=int(time.time()),
+    )
+    logger.info(data)
+    return data
+
+
+loop = asyncio.new_event_loop()
+asyncio.set_event_loop(loop)
+loop.run_until_complete(run())
